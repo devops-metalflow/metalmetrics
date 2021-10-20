a0cde8af553251e09e74382aba6390a5b4bc421f
Jia Jia <angersax@sina.com>
2021-10-20 10:23:18 +0800

    Add module of cmd

---
+++ src/cmd/argument.rs
@@ -0,0 +1,73 @@
+use clap::{App, Arg};
+
+pub struct Argument {
+    pub config_file: String,
+    pub inxi_file: String,
+    pub listen_url: String,
+    pub output_file: String,
+}
+
+impl Argument {
+    pub fn parse(&mut self) {
+        let matches = App::new("metalmetrics-rs")
+            .version(concat!(env!("CARGO_PKG_VERSION"), "-build-", env!("build")))
+            .arg(
+                Arg::new("config_file")
+                    .short('c')
+                    .long("config-file")
+                    .value_name("NAME")
+                    .about("config file (.yml)")
+                    .takes_value(true)
+                    .required(true),
+            )
+            .arg(
+                Arg::new("inxi_file")
+                    .short('i')
+                    .long("inxi-file")
+                    .value_name("NAME")
+                    .about("inxi file (/path/to/inxi)")
+                    .takes_value(true)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("listen_url")
+                    .short('l')
+                    .long("listen-url")
+                    .value_name("URL")
+                    .about("listen url (host:port)")
+                    .takes_value(true)
+                    .required(false),
+            )
+            .arg(
+                Arg::new("output_file")
+                    .short('o')
+                    .long("output-file")
+                    .value_name("NAME")
+                    .about("output file (.json|.txt|.xlsx)")
+                    .takes_value(true)
+                    .required(false)
+                    .conflicts_with("listen_url"),
+            )
+            .get_matches();
+
+        match matches.value_of("config_file") {
+            Some(name) => self.config_file = name.to_string(),
+            None => self.config_file = "".to_string(),
+        }
+
+        match matches.value_of("inxi_file") {
+            Some(name) => self.inxi_file = name.to_string(),
+            None => self.inxi_file = "".to_string(),
+        }
+
+        match matches.value_of("listen_url") {
+            Some(url) => self.listen_url = url.to_string(),
+            None => self.listen_url = "".to_string(),
+        }
+
+        match matches.value_of("output_file") {
+            Some(name) => self.output_file = name.to_string(),
+            None => self.output_file = "".to_string(),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,12 @@
-use clap::{App, Arg};
+mod cmd;
 
 fn main() {
-    let app = App::new("metalmetrics-rs")
-        .version(concat!(env!("CARGO_PKG_VERSION"), "-build-", env!("build")))
-        .arg(
-            Arg::new("config_file")
-                .short('c')
-                .long("config-file")
-                .value_name("NAME")
-                .about("config file (.yml)")
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::new("inxi_file")
-                .short('i')
-                .long("inxi-file")
-                .value_name("NAME")
-                .about("inxi file (/path/to/inxi)")
-                .takes_value(true)
-                .required(false),
-        )
-        .arg(
-            Arg::new("listen_url")
-                .short('l')
-                .long("listen-url")
-                .value_name("URL")
-                .about("listen url (host:port)")
-                .takes_value(true)
-                .required(false),
-        )
-        .arg(
-            Arg::new("output_file")
-                .short('o')
-                .long("output-file")
-                .value_name("NAME")
-                .about("output file (.json|.txt|.xlsx)")
-                .takes_value(true)
-                .required(false)
-                .conflicts_with("listen_url"),
-        )
-        .get_matches();
+    let mut arg = cmd::argument::Argument {
+        config_file: "".to_string(),
+        inxi_file: "".to_string(),
+        listen_url: "".to_string(),
+        output_file: "".to_string(),
+    };
+
+    arg.parse();
 }
