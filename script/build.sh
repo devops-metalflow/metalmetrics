185a52338dde2ec6bedb59db72b4fdb879990bd8
Jia <49358172+craftslab@users.noreply.github.com>
2021-11-26 14:08:39 +0800

    Update build.sh

--- script/build.sh
+++ script/build.sh
@@ -8,6 +8,8 @@ windows="target/x86_64-pc-windows-gnu/release/metalmetrics-rs.exe"
 if [ "$1" = "all" ]; then
   build=$build cargo build --release --all-features --all-targets --target x86_64-pc-windows-gnu
   build=$build cargo build --release --all-features --all-targets --target x86_64-unknown-linux-gnu
+elif [ "$1" = "offline" ]; then
+  build=$build cargo build --release --all-features --all-targets --offline
 elif [ "$1" = "check" ]; then
   build=$build cargo check --release --all-features --all-targets
 else
